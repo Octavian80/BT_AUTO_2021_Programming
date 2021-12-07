07f4ded13e9e0f45e58f35b0fdaa7aee02c60d3d
Octavian80 <orosromum@gmail.com>
2021-12-07 21:16:51 +0200

    Update

---
+++ BT_AUTO_2021_Programming/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BT_AUTO_2021_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ana are mere");
+        }
+    }
+}
