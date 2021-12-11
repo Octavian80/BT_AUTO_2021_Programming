17c7bd2c463151b74c60a50ae28848bb674bcb56
Octavian80 <orosromum@gmail.com>
2021-12-11 23:27:24 +0200

    Update course 10.12.2021

--- BT_AUTO_2021_Programming/Program.cs
+++ BT_AUTO_2021_Programming/Program.cs
@@ -6,8 +6,227 @@ namespace BT_AUTO_2021_Programming
     {
         static void Main(string[] args)
         {
+            const int MAX_SIZE = 100;
+            const double PI = 3.141592;
+            const int MAX_NUMBER = 5;
+            
             Console.WriteLine("Hello World!");
             Console.WriteLine("Ana are mere");
+
+            int numberOfStudents = 5;
+            int count;
+            int a, b;
+            a = numberOfStudents + 1;
+            a = a + numberOfStudents;
+            short s = 5;
+            ulong ul = 65;
+
+            float numberFloat = 5.6F;
+            double numberDouble = 5.6;
+
+            float f = (float)numberDouble;
+            int numberInt=(int)f;
+
+
+
+
+            Console.WriteLine(typeof(int).IsPrimitive);
+
+            bool myBoolean = false;
+
+            char ch = 'd'; //'1' != 1;
+
+            string myString = "Ana are mere si pere";
+            string result = myString + " si struguri";
+            Console.WriteLine(result);
+
+            DateTime dataCurenta = new DateTime(2021, 12, 8);
+            Console.WriteLine(dataCurenta);
+
+            Console.WriteLine(numberFloat * a);
+            Console.WriteLine(5 / 6.0);
+            Console.WriteLine(5 % 6);
+
+            int x = 2;
+            x += 5;//x-x+5;
+            //x++<=>x=x+1
+            //++x <=>x=x+1
+            Console.WriteLine(++x);
+            Console.WriteLine(!(x == a));
+
+            int number = 8;
+            Console.WriteLine("Number tested is" + number);
+
+            if (number >=0)
+            {
+                Console.WriteLine("Number is positive!");    
+                    
+                    }
+
+            else
+            { 
+                Console.WriteLine("Number is negative");
+            }
+
+            if(number % 2== 0)
+            {
+                Console.WriteLine("Even number!");
+            }
+            else
+            {
+                Console.WriteLine("Odd number!");
+            }
+
+            if(number <= 40 && number >=0 )
+            {
+                if (number<=20)
+                {
+                    Console.WriteLine("Student failed!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Student passed!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Grade is invalid!");
+            }
+
+            //One line if/else
+            String message=(number % 2==0) ? "Even":"Odd";
+            Console.WriteLine(message);
+
+            if (number == 1)
+            {
+                Console.WriteLine("Monday");
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            if(number == 3)
+            {
+                Console.WriteLine ("Wednesday");
+            }
+            if(number==5)
+            {
+                Console.WriteLine ("Thursday");
+
+            }
+            if(number==5)
+            {
+                Console.WriteLine ("Friday");
+            }
+            if(number==6)
+            {
+                Console.WriteLine ("Saturday");
+            }
+            if(number==7)
+            {
+                Console.WriteLine ("Sunday");
+            }
+            if(number<1 || number >7)
+            {
+                Console.WriteLine("Sorry not a valid day!");
+            }
+
+            switch(number)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Monday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Tuesday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Wednesday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Thursday");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Friday");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Saturday");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Sunday");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Not a valid day!");
+                        break;
+                    }
+            }
+
+            int counter = 0;
+            while (counter <=MAX_NUMBER)
+            {
+                Console.WriteLine("Current number is:" + counter);
+                counter++;
+            }
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine("Current number is:" + counter);
+                counter++;
+            }
+            while (counter <= 5);
+
+            for (counter=0;counter <=MAX_NUMBER;counter++)
+            {
+                //Console.WriteLine("Current number is:" + counter);
+                Console.WriteLine("Current number is {0}", counter);
+            }
+
+            foreach (string argument in args)
+            {
+                Console.WriteLine("The argument is: {0}", argument );
+            }
+
+            for (counter = 0; counter < args.Length; counter++)
+            {
+                Console.WriteLine("The argument is:{0}", args[counter]);
+            }
+
+            int year = 1900;
+            int febDays = 28;
+
+            if(year % 4 == 0)
+            {
+                if(year % 100==0 && year % 400 !=0)
+                {
+                    febDays = 29;
+                }
+                
+                if(year %100 !=0)
+                {
+                    febDays = 29;
+                }
+             
+
+            }
+            
+            Console.WriteLine("February has {0} days" , febDays);
+
         }
     }
 }
