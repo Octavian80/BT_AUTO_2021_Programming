3527c3b8d9dd7db8c50087d174df988c9e3e0003
Octavian80 <orosromum@gmail.com>
2022-01-09 21:06:08 +0200

    Person exercises, collections

---
+++ BT_AUTO_2021_Programming/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT_AUTO_2021_Programming
+{
+    class Employee :Person
+    {
+        double salary;
+
+        public Employee(string firstName, string lastName, string adress, string city, string country, double salary) : base(firstName, lastName, adress, city, country)
+        {
+            this.salary = salary;
+        }
+        public void SetSalary( double salary)
+        {
+            this.salary = salary;
+        }
+
+        public static void PrintPersonStatic(Employee p)
+        {
+            Console.WriteLine("Employee first name is {0} and last name is {1} with salary {2}", p.GetFirstName(), p.GetLastName(), p.salary);
+        }
+    }
+}
